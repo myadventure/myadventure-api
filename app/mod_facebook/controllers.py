550409c0c47976f897b043604fffefb1af2768a9
Martin Spier <spiermar@gmail.com>
2015-12-26 13:27:11 -0800

    Moving facebook configuration to same config file.

--- app/mod_facebook/controllers.py
+++ app/mod_facebook/controllers.py
@@ -11,24 +11,30 @@ from mongoengine import DoesNotExist
 
 from app.mod_user.models import User
 
-config = ConfigParser.ConfigParser()
-config.readfp(open('facebook.config'))
-
 mod_facebook = Blueprint('facebook', __name__, url_prefix='/facebook')
 
 oauth = client.OAuth(current_app)
 
-facebook = oauth.remote_app(
-    'facebook',
-    consumer_key=config.get('facebook', 'APP_ID'),
-    consumer_secret=config.get('facebook', 'APP_SECRET'),
-    request_token_params={'scope': 'email'},
-    base_url='https://graph.facebook.com',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    access_token_method='GET',
-    authorize_url='https://www.facebook.com/dialog/oauth'
-)
+facebook = None
+
+@mod_facebook.before_app_first_request
+def register_facebook():
+    facebook = oauth.remote_app(
+        'facebook',
+        consumer_key=current_app.config['FACEBOOK_APP_ID'],
+        consumer_secret=current_app.config['FACEBOOK_APP_SECRET'],
+        request_token_params={'scope': 'email'},
+        base_url='https://graph.facebook.com',
+        request_token_url=None,
+        access_token_url='/oauth/access_token',
+        access_token_method='GET',
+        authorize_url='https://www.facebook.com/dialog/oauth'
+    )
+
+
+    @facebook.tokengetter
+    def get_facebook_oauth_token():
+        return session.get('oauth_token')
 
 
 @mod_facebook.route('/login')
@@ -82,11 +88,3 @@ def facebook_authorized():
 def me():
     me = facebook.get('/me?fields=id,name,email,link')
     return Response('Logged in as id=%s name=%s email=%s redirect=%s' % (me.data.get('id'), me.data.get('name'), me.data.get('email'), request.args.get('next')))
-
-
-@facebook.tokengetter
-def get_facebook_oauth_token():
-    return session.get('oauth_token')
-
-
-
