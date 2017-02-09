3376b8d7391f2d82221e225c1d6cfee42bae1c47
Martin Spier <spiermar@gmail.com>
2017-02-09 10:54:38 -0800

    Loading instagram media.

--- app/views/instagram.py
+++ app/views/instagram.py
@@ -27,12 +27,15 @@ def get_media(api, media=None, max_id=None):
     if media is None:
         media = []
 
-    response, next = api.user_recent_media(max_id=max_id)
+    response, next_url = api.user_recent_media(max_id=max_id)
 
     for media_item in response:
         media.append(media_item)
 
-    if next is not None:
+    # pagination is not working as expected.
+    # the client is expecting a next_url parameter in the pagination object,
+    # but even with more pictures to load, nothing is being returned.
+    if next_url is not None:
         parsednext = urlparse.urlparse(next)
         max_id = urlparse.parse_qs(parsednext.query)['max_id'][0]
         media = get_media(api, media, max_id)
@@ -54,6 +57,8 @@ def import_media(access_token, client_secret, adventure):
         title = None
         desc = None
         video = None
+        latitude = None
+        longitude = None
         if hasattr(item, 'caption'):
             if item.caption is not None:
                 desc = item.caption.text
@@ -64,6 +69,9 @@ def import_media(access_token, client_secret, adventure):
                 latitude = item.location.point.latitude
                 longitude = item.location.point.longitude
         timestamp = item.created_time
+        media_type = item.type
+        if media_type == 'image':
+            media_type = 'photo'
         thumb = item.images.get('thumbnail').url
         photo = item.images.get('standard_resolution').url
         if hasattr(item, 'videos'):
@@ -72,7 +80,7 @@ def import_media(access_token, client_secret, adventure):
 
         if item.id is not None:
             point = adventure.points.filter( \
-                point_type='photo', instagram_id=item.id \
+                point_type=media_type, instagram_id=item.id \
             ).first()
 
             if point is None:
@@ -85,7 +93,7 @@ def import_media(access_token, client_secret, adventure):
                     latitude=latitude,
                     longitude=longitude,
                     resource=resource,
-                    point_type='photo',
+                    point_type=media_type,
                     timestamp=timestamp,
                     delorme_id=None,
                     aio_id=None,
